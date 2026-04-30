629e90e7f3fcc14cc6cd2849dbf1a0a6fc7fa7c2
Ayush Kelkar <ayush94klkr@gmail.com>
2026-04-30 18:31:21 +0530

    atlas works

--- index.js
+++ index.js
@@ -1,10 +1,21 @@
 const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors');
+require('dotenv').config();
+
 const app = express();
 
+app.use(cors());
+app.use(express.json());
+
+mongoose.connect(process.env.MONGO_URI)
+  .then(() => console.log('MongoDB connected'))
+  .catch((err) => console.log(err));
+
 app.get('/', (req, res) => {
-  res.send('Hello World');
+  res.send('CASS API is running');
 });
 
-app.listen(3000, () => {
-  console.log('Server running on port 3000');
+app.listen(process.env.PORT, () => {
+  console.log(`Server running on port ${process.env.PORT}`);
 });
